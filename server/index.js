3f31f944d3031662313fea30815f26395abde878
Hugo Xiong <hugoxiong@gmail.com>
2020-10-10 12:06:41 -0700

    load in and render photo component

--- server/index.js
+++ server/index.js
@@ -8,6 +8,10 @@ app.use(express.static(path.join(__dirname, '../public')));
 // port 4000 for the proxy server
 const port = 4000;
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+})
+
 app.listen(port, () => {
   console.log(`Hugo's proxy server listening on ${port}`);
 });
