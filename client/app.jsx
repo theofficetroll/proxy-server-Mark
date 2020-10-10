cfddd8635409ec7155567ca7d205773de7b3dcb9
Hugo Xiong <hugoxiong@gmail.com>
2020-10-10 12:48:10 -0700

    add skeleton header/footer components

---
+++ client/app.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'core-js/stable';
+
+import './app.css';
+
+import Header from './components/Header.jsx';
+import Footer from './components/Footer.jsx';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Header />
+  </React.StrictMode>,
+  document.getElementById('header'),
+);
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Footer />
+  </React.StrictMode>,
+  document.getElementById('footer'),
+);
--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const SRC_DIR = path.join(__dirname, '/client');
 const DIST_DIR = path.join(__dirname, '/public');
 
 module.exports = {
-  entry: `${SRC_DIR}/index.jsx`,
+  entry: `${SRC_DIR}/app.jsx`,
   output: {
     filename: 'bundle-proxy.js',
     path: DIST_DIR,
