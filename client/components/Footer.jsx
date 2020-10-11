80a6051d658ead41ef534e3fbcfac0020bc160b9
Hugo Xiong <hugoxiong@gmail.com>
2020-10-11 00:57:43 -0700

    add footer body container styling

--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -5,7 +5,13 @@ import styles from "./Footer.module.css";
 const Footer = () => {
     return (
       <footer>
-        <div className={styles.footerContainer}></div>
+        <div className={styles.footerContainer}>
+          <div className={styles.body}>
+            <div className={styles.linksMenu}></div>
+            <div className={styles.socialMenu}></div>
+          </div>
+          <div className={styles.subFooter}></div>
+        </div>
       </footer>
     )
 }
