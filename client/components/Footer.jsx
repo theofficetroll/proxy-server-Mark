80125e1bd140246d6d5679d0fe87a26bee7bf16b
Hugo Xiong <hugoxiong@gmail.com>
2020-10-11 16:29:56 -0700

    add basic css styling to footer

--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -1,48 +1,64 @@
 import React from 'react';
 
+import FooterAccordion from "./FooterAccordion.jsx";
 import styles from "./Footer.module.css";
 
-const links = ["GIFT CARDS", "PROMOTIONS", "FIND A STORE", "SIGN UP FOR EMAIL", "BECOME A MEMBER", "NIKE JOURNAL", "SEND US FEEDBACK"];
+const links1 = ["GIFT CARDS", "PROMOTIONS", "FIND A STORE", "SIGN UP FOR EMAIL", "BECOME A MEMBER", "NIKE JOURNAL", "SEND US FEEDBACK"];
+
+const links2 = ["GET HELP", "Order Status", "Shipping and Delivery", "Returns", "Payment Options", "Gift Card Balance", "Contact Us"];
+
+const links3 = ["ABOUT NIKE", "News", "Careers", "Investors", "Purpose", "Sustainability", "CA Supply Chains Act"];
 
 const Footer = () => {
-
-  const accordionItems = links.map((link, index) => {
-    return (
-      <>
-            <div className={styles.accordionHeader}></div>
-        <div className={styles.accordionBody}>
-          <ul>
-            <li className={styles.footerLink} key={index}>
-              <a>{link}</a>
-            </li>
-          </ul>
-        </div>
-      </>
-
-      )
-  });
-
     return (
       <footer>
         <div className={styles.footerContainer}>
           <div className={styles.footerBody}>
             <div className={styles.linksMenu}>
-              <div className={`${styles.accordion} ${styles.first}`}>
-                {accordionItems}
-              </div>
+              <FooterAccordion links={links1} />
+              <FooterAccordion links={links2} />
+              <FooterAccordion links={links3} />
             </div>
-            <div className={styles.socialMenu}></div>
+            <div className={styles.socialMenu}>
+              <ul>
+                <li>
+                  <a className={styles.footerLink}>T</a>
+                </li>
+                <li>
+                  <a className={styles.footerLink}>F</a>
+                </li>
+                <li>
+                  <a className={styles.footerLink}>Y</a>
+                </li>
+                <li>
+                  <a className={styles.footerLink}>I</a>
+                </li>
+              </ul>
+            </div>
           </div>
           <div className={styles.subFooter}>
             <div>
-              <span>United States</span>
-              <span>© 2020 Nike, Inc. All Rights Reserved</span>
+              <a className={styles.countryPin}>
+                <span>PIN</span>
+                <span>United States</span>
+              </a>
+              <div className={styles.copyright}>© 2020 Nike, Inc. All Rights Reserved</div>
             </div>
             <div>
-              <span>Guides</span>
-              <span>Terms of Sale</span>
-              <span>Terms of Use</span>
-              <span>Nike Privacy Policy</span>
+              <ul className={styles.subFooterMenu}>
+                <li>
+                  <div className={styles.footerLink}>Guides [MENU]</div>
+                </li>
+                <li>
+                  <a className={styles.footerLink}>Terms of Sale</a>
+                </li>
+                <li>
+                  <a className={styles.footerLink}>Terms of Use</a>
+                </li>
+                <li>
+                  <a className={styles.footerLink}>Nike Privacy Policy</a>
+                </li>
+              </ul>
             </div>
           </div>
         </div>
--- client/components/FooterAccordion.jsx
+++ client/components/FooterAccordion.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
 
-const FooterAccordion = ( {name} ) => {
+import styles from "./Footer.module.css";
 
+const FooterAccordion = ( {links} ) => {
+  const linkItems = links.map((link, index) => (
+    <li className={styles.footerLink} key={index}>
+      <a>{link}</a>
+    </li>
+  ));
+
+  return (
+    <div className={`${styles.accordion}`}>
+      <div className={styles.accordionHeader}></div>
+      <div className={styles.accordionBody}>
+        <ul>
+          {linkItems}
+        </ul>
+      </div>
+    </div>
+  )
 }
 
 export default FooterAccordion;
