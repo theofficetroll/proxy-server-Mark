a57632bcd0242f5de929625d7fec32c6ab80ef09
Hugo Xiong <hugoxiong@gmail.com>
2020-10-21 22:21:22 -0700

    change port back to 4000

--- server/index.js
+++ server/index.js
@@ -15,8 +15,8 @@ app.get('*', (req, res) => {
   res.status(404).send();
 });
 
-// port 80 for the proxy server
-const port = 80;
+// port 4000 for the proxy server
+const port = 4000;
 
 app.listen(port, () => {
   console.log(`Hugo's proxy server listening on port ${port}`);
