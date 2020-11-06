fc5d24f03867188380f2d379a02d789947739194
Hugo Xiong <hugoxiong@gmail.com>
2020-11-05 19:53:44 -0800

    add review service proxy route

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const morgan = require('morgan');
+const fetch = require('node-fetch');
 
 const app = express();
 
@@ -9,6 +10,21 @@ app.use(express.static(path.join(__dirname, '../public')));
 
 app.get('/shop/:productId/:styleId', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+app.get('/api/reviews/:productID', async (req, res) => {
+  const { productID } = req.params;
+  try {
+    const response = await fetch(
+      `http://18.219.146.205:3003/api/reviews/${productID}`
+    );
+    console.log(response);
+    const data = await response.json();
+    console.log(data);
+    res.status(200).send(data);
+  } catch (err) {
+    res.status(500).send(err);
+  }
 });
 
 app.get('*', (req, res) => {
