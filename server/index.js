2dcde9fa77e11f8b5408bdc1d02d61b830cd2d7a
Mark <mark.scrudder@gmail.com>
2020-12-11 12:10:13 -0800

    Fixed reverse proxy.

--- server/index.js
+++ server/index.js
@@ -1,9 +1,14 @@
+require('newrelic');
+
 const express = require('express');
 const path = require('path');
 const morgan = require('morgan');
 const fetch = require('node-fetch');
 
 const app = express();
+
+// port 4000 for the proxy server
+const port = 4000;
 
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, '../public')));
@@ -18,7 +23,20 @@ app.get('/api/reviews/:productID', async (req, res) => {
     const response = await fetch(
       `http://18.219.146.205:3003/api/reviews/${productID}`
     );
-    console.log(response);
+    const data = await response.json();
+    console.log(data);
+    res.status(200).send(data);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+app.get('/api/product/:productID', async (req, res) => {
+  const { productID } = req.params;
+  try {
+    const response = await fetch(
+      `http://127.0.0.1:3008/product/${productID}`
+    );
     const data = await response.json();
     console.log(data);
     res.status(200).send(data);
@@ -31,8 +49,7 @@ app.get('*', (req, res) => {
   res.status(404).send();
 });
 
-// port 4000 for the proxy server
-const port = 4000;
+
 
 app.listen(port, () => {
   console.log(`Hugo's proxy server listening on port ${port}`);
