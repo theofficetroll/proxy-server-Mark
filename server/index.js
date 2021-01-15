24755a7b9c63c8d1df7dea4ded667d0a16eba1e6
Mark <mark.scrudder@gmail.com>
2021-01-14 22:08:20 -0800

    Commit to push current version.

--- server/index.js
+++ server/index.js
@@ -10,6 +10,21 @@ const app = express();
 // port 4000 for the proxy server
 const port = 4000;
 
+// addresses
+const redisURL = "3.139.103.221";
+const productLocal = "http://127.0.0.7:3008";
+
+// redis setup
+// const redis = require('redis');
+// const redisClient = redis.createClient({ host: "localhost", port: 6379 });
+// redisClient.on('connect', function() {
+//   console.log('Redis is connected');
+// });
+
+// redisClient.on('error', function*() {
+//   console.log('Error in Redis');
+// });
+
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, '../public')));
 
@@ -17,31 +32,27 @@ app.get('/shop/:productId/:styleId', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
-app.get('/api/reviews/:productID', async (req, res) => {
-  const { productID } = req.params;
-  try {
-    const response = await fetch(
-      `http://18.219.146.205:3003/api/reviews/${productID}`
-    );
-    const data = await response.json();
-    console.log(data);
-    res.status(200).send(data);
-  } catch (err) {
-    res.status(500).send(err);
-  }
-});
-
 app.get('/api/product/:productID', async (req, res) => {
-  const { productID } = req.params;
   try {
-    const response = await fetch(
-      `http://127.0.0.1:3008/product/${productID}`
-    );
-    const data = await response.json();
-    console.log(data);
-    res.status(200).send(data);
-  } catch (err) {
-    res.status(500).send(err);
+    const { productID } = req.params;
+    // redisClient.get(productID, async (err, data) => {
+    //   if (err) throw err;
+
+    //   if (data) {
+    //     console.log('found on redis');
+    //     res.status(200).send(data);
+    //   } else {
+        const response = await fetch(
+          `${productLocal}/product/${productID}`
+        );
+        const data = await response.json();
+    //     res.status(200).send(data);
+    //   }
+    // });
+  }
+  catch(err) {
+    console.log(err);
+    res.status(400).send(err);
   }
 });
 
